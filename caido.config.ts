0315cbb42a531766ecd553d7965c20060d09059e
Emile Fugulin <code@efugulin.com>
2025-07-16 11:59:33 -0400

    First attempt at upgrading the repo

--- caido.config.ts
+++ caido.config.ts
@@ -1,22 +1,89 @@
+import path from "path";
+
+import tailwindCaido from "@caido/tailwindcss";
 import { defineConfig } from "@caido-community/dev";
+import vue from "@vitejs/plugin-vue";
+import prefixwrap from "postcss-prefixwrap";
+import tailwindcss from "tailwindcss";
+// @ts-expect-error no declared types at this time
+import tailwindPrimeui from "tailwindcss-primeui";
 
 export default defineConfig({
   id: "shift",
   name: "Shift",
   description: "Seamless AI Integration into Caido.",
-  version: "0.0.0",
+  version: "0.2.0",
   author: {
-    name: "JThacker & CRITSoftware",
-    email: "justin@criticalthinkingpodcast.io",
-    url: "https://github.com/CRITSoftware/shift",
+    name: "Caido Labs Inc.",
+    email: "dev@caido.io",
+    url: "https://caido.io",
   },
-  plugins: [ 
+  plugins: [
     {
-      "kind": "frontend",
-      "id": "frontend",
-      "name": "Frontend",
-      "root": "packages/frontend",
-    //   "style": "frontend/style.css"
-    }
+      kind: "frontend",
+      id: "frontend",
+      name: "Frontend",
+      root: "packages/frontend",
+      vite: {
+        plugins: [vue()],
+        build: {
+          rollupOptions: {
+            external: [
+              "@caido/frontend-sdk",
+              "@codemirror/autocomplete",
+              "@codemirror/commands",
+              "@codemirror/language",
+              "@codemirror/lint",
+              "@codemirror/search",
+              "@codemirror/state",
+              "@codemirror/view",
+              "@lezer/common",
+              "@lezer/highlight",
+              "@lezer/lr",
+              "vue",
+            ],
+          },
+        },
+        resolve: {
+          alias: [
+            {
+              find: "@",
+              replacement: path.resolve(__dirname, "packages/frontend/src"),
+            },
+          ],
+        },
+        css: {
+          postcss: {
+            plugins: [
+              // This plugin wraps the root element in a unique ID
+              // This is necessary to prevent styling conflicts between plugins
+              prefixwrap(`#plugin--${id}`, {
+                blacklist: [".*Global.css"],
+              }),
+
+              tailwindcss({
+                corePlugins: {
+                  preflight: false,
+                },
+                content: [
+                  "./packages/frontend/src/**/*.{vue,ts}",
+                  "./node_modules/@caido/primevue/dist/primevue.mjs",
+                ],
+                // Check the [data-mode="dark"] attribute on the <html> element to determine the mode
+                // This attribute is set in the Caido core application
+                darkMode: ["selector", '[data-mode="dark"]'],
+                plugins: [
+                  // This plugin injects the necessary Tailwind classes for PrimeVue components
+                  tailwindPrimeui,
+
+                  // This plugin injects the necessary Tailwind classes for the Caido theme
+                  tailwindCaido,
+                ],
+              }),
+            ],
+          },
+        },
+      },
+    },
   ],
 });
